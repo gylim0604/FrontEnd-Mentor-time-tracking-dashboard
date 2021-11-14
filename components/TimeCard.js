dc345eff85473e50dfdad6ac570d28f5b70e6662
gylim0604 <gylim0604@hotmail.com>
2021-11-14 21:51:59 +0800

    completed project, need to update README

--- components/TimeCard.js
+++ components/TimeCard.js
@@ -59,7 +59,16 @@ function TimeCard({ state }) {
                 <span className={styles.title}>
                     <h3>{title}</h3>
                     <a href='#'>
-                        <img as='svg' src='/images/icon-ellipsis.svg' alt='' />
+                        <svg
+                            width='21'
+                            height='5'
+                            xmlns='http://www.w3.org/2000/svg'
+                        >
+                            <path
+                                d='M2.5 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Zm8 0a2.5 2.5 0 1 1 0 5 2.5 2.5 0 0 1 0-5Z'
+                                fillRule='evenodd'
+                            />
+                        </svg>
                     </a>
                 </span>
                 <span className={styles.tracker}>
